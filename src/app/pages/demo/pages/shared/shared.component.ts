67666b505f786aee0f3584af352f5b6937af4ceb
Enzo Facundo Orbe <Enzo.Orbe@tsoftglobal.com>
2022-02-21 18:16:21 -0300

    Creacion de mini formulario

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validator } from '@angular/forms';
 @Component({
   selector: 'app-shared',
   templateUrl: './shared.component.html',
-  styleUrls: ['./shared.component.scss']
+  styleUrls: ['./shared.component.scss'],
 })
 export class SharedComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      input: [null],
+    });
   }
 
+  onPatchValue(): void {
+    this.form.patchValue({ input: 'Enzo Orbe' });
+  }
+
+  onSubmit(): void {
+    console.log('Presiono boton submit ', this.form.value);
+  }
 }
